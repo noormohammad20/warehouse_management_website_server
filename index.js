4992126fa62c1f70d1611d1abb27b9a6ba51e6d7
noormohammad20 <mdsaim204@gmail.com>
2022-04-29 14:44:48 +0600

    create dynamic api for item id

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
 const app = express()
 const port = process.env.PORT || 5000
 
@@ -22,6 +22,13 @@ async function run() {
             const cursor = itemsCollections.find(query)
             const items = await cursor.toArray()
             res.send(items)
+        })
+
+        app.get('/inventoryItems/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const item = await itemsCollections.findOne(query)
+            res.send(item)
         })
     }
     finally {

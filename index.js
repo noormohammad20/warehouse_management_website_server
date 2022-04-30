f1a8e68298c6725794d0aaafc9a6c50c7f59d4fd
noormohammad20 <mdsaim204@gmail.com>
2022-05-01 00:15:41 +0600

    create a delete api

--- index.js
+++ index.js
@@ -16,22 +16,22 @@ async function run() {
     try {
         await client.connect()
         const itemsCollections = client.db('toyWarehouse').collection('items')
-
+        //get method for get all items
         app.get('/inventoryItems', async (req, res) => {
             const query = {}
             const cursor = itemsCollections.find(query)
             const items = await cursor.toArray()
             res.send(items)
         })
-
+        //get method for get one item by id
         app.get('/inventoryItems/:id', async (req, res) => {
             const id = req.params.id
             const query = { _id: ObjectId(id) }
             const item = await itemsCollections.findOne(query)
             res.send(item)
         })
-
-        app.put('/inventoryItems/update/:id', async (req, res) => {
+        //put method for decrease quantity
+        app.put('/inventoryItems/:id', async (req, res) => {
             const id = req.params.id
             const { newQuantity } = req.body
             console.log(newQuantity)
@@ -43,6 +43,31 @@ async function run() {
                 }
             }
             const result = await itemsCollections.updateOne(filter, updateDoc, options)
+            res.send(result)
+        })
+        //put method for increase quantity (not working)
+        app.put('/inventoryItems/:id', async (req, res) => {
+            const id = req.params.id
+            console.log(req.body)
+            const { renewQuantity } = req.body
+            console.log(renewQuantity)
+            const filter = { _id: ObjectId(id) }
+            const options = { upsert: true }
+            const updateDoc = {
+                $set: {
+                    quantity: renewQuantity
+                }
+            }
+            const result = await itemsCollections.updateOne(filter, updateDoc, options)
+            res.send(result)
+        })
+
+        //delete method for delete item in manage inventory
+
+        app.delete('/inventoryItems/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const result = await itemsCollections.deleteOne(query)
             res.send(result)
         })
 

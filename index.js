9f769df14cca7685b75ffb2073c9de3ad0273576
noormohammad20 <mdsaim204@gmail.com>
2022-05-01 21:47:15 +0600

    added jwt for verify email

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
+const jwt = require('jsonwebtoken')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
 const app = express()
 const port = process.env.PORT || 5000
@@ -9,6 +10,23 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+    const token = authHeader.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'forbidden' })
+        }
+        console.log('decoded', decoded)
+        req.decoded = decoded
+        next()
+    })
+
+}
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.acdjr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 })
@@ -16,7 +34,16 @@ async function run() {
     try {
         await client.connect()
         const itemsCollections = client.db('toyWarehouse').collection('items')
-        const myItemsCollections = client.db('toyWarehouse').collection('myItems')
+
+        //AUTH
+        app.post('/login', async (req, res) => {
+            const user = req.body
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            })
+            res.send({ accessToken })
+        })
+
         //get method for get all items
         app.get('/inventoryItems', async (req, res) => {
             const query = {}
@@ -81,13 +108,18 @@ async function run() {
         })
 
         // get method to  manage my Items
-        app.get('/myItems', async (req, res) => {
+        app.get('/myItems', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email
             const email = req.query.email
-            console.log(email)
             const query = { email: email }
-            const cursor = itemsCollections.find(query)
-            const items = await cursor.toArray()
-            res.send(items)
+            if (email === decodedEmail) {
+                const cursor = itemsCollections.find(query)
+                const items = await cursor.toArray()
+                res.send(items)
+            }
+            else {
+                res.status(403).send({ message: 'forbidden' })
+            }
         })
 
     }
